7b23a26b1c226afcf158f25ea8d0483d4859e082
Schyzophrenic <pierre.chastagner@gmail.com>
2022-05-18 17:44:05 +0100

    Readme update

--- source/carrot.lua
+++ source/carrot.lua
@@ -1,3 +1,5 @@
+import "CoreLibs/sprites"
+
 local pd <const> = playdate
 local gfx <const> = pd.graphics
 
@@ -11,4 +13,12 @@ function Carrot:init(x, y)
     self:setImage(carrotImg)
     self:moveTo(x, y)
     self:add()
+
+    -- we set a collision box for collection
+    self:setCollideRect(0, 0, self.width, self.height)
+end
+
+function Carrot:update()
+    -- If there is a collision it has to be with the rabbit so we remove the carrot
+    
 end
